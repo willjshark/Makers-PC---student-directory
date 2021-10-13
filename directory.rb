c2f784eac0184c26e918e203ed6c3dc053d7fbe6
willjshark <willjshark@gmail.com>
2021-10-13 13:28:12 +0200

    Directory with user input

--- directory.rb
+++ directory.rb
@@ -1,19 +1,3 @@
-#This array contains a listof all current students
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
- ]
-
-
 def print(names)
   puts "The students of Villains Academy"
   puts "-------------"
@@ -26,5 +10,23 @@ def print_footer(names)
   puts "Overall, we have #{names.length} great students. "
 end
 
+def input_students 
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice" 
+
+  students = []
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students" 
+    name = gets.chomp
+  end 
+
+  students
+
+end
+
+students = input_students
 print(students)
 print_footer(students)

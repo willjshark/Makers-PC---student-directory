2459a0d3922a00bf578cffd2ce5d850a8475d38e
willjshark <willjshark@gmail.com>
2021-10-14 19:19:48 +0200

    Directory with methods

--- directory.rb
+++ directory.rb
@@ -1,32 +1,66 @@
-def print(names)
+def print_students_list
   puts "The students of Villains Academy"
   puts "-------------"
-  names.each do |student|
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)" 
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.length} great students. "
+def print_footer
+  puts "Overall, we have #{@students.length} great students. "
 end
+
+
 
 def input_students 
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice" 
 
-  students = []
   name = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students" 
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students" 
     name = gets.chomp
   end 
-
-  students
-
 end
 
-students = input_students
-print(students)
-print_footer(students)
+
+
+def interactive_menu
+  @students = []
+  loop do
+    print_menu
+	process(gets.chomp)
+  end
+end
+   
+
+def process(selection) 
+    
+	case selection
+      when "1"
+	     input_students
+	  when "2"
+	     show_students
+	  when "9"
+	     exit
+	  else 
+	    puts "I don't know what you meant, try again"
+	  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"  
+end
+
+def show_students
+  print_students_list
+  print_footer
+end
+
+
+
+interactive_menu

16352fb469b5d5574faa81b33172be71f41adc2c
willjshark <willjshark@gmail.com>
2021-10-17 13:12:17 +0200

    Students save file

--- directory.rb
+++ directory.rb
@@ -43,6 +43,8 @@ def process(selection)
 	     input_students
 	  when "2"
 	     show_students
+	  when "3"
+	      save_students
 	  when "9"
 	     exit
 	  else 
@@ -53,6 +55,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to student.csv"
   puts "9. Exit"  
 end
 
@@ -60,6 +63,18 @@ def show_students
   print_students_list
   print_footer
 end
+
+def save_students
+
+  file = File.open("students.csv", "a")
+  
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+	csv_line = student_data.join(",")
+	file.puts csv_line
+   end
+   file.close
+ end
 
 
 
--- directory2.rb
+++ directory2.rb
@@ -2,7 +2,7 @@ def print(names)
   puts "The students of Villains Academy"
   puts "-------------"
   names.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" 
+     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" 
   end
 end
 
@@ -16,9 +16,11 @@ def input_students
 
   students = []
   name = gets.chomp
+  puts "Please choose the starting letter of the names you want to print"
+  letter = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november} if name.start_with? 'letter'
     puts "Now we have #{students.count} students" 
     name = gets.chomp
   end 
@@ -30,6 +32,8 @@ end
 
 
 
+
+
 students = input_students
 print(students)
-print_footer(students)
+#print_footer(students)
